9896d54f44ee2184cc6b72b25e136fcc1cefd053
Talha Arshad <talha.arshad@systemsltd.com>
2022-09-08 03:47:59 +0500

    import one class from another

---
+++ Animal.java
@@ -0,0 +1,5 @@
+public class Animal {
+    public static String iMCat(){
+        return "I am a Cat";
+    }
+}
---
+++ lesson1.java
@@ -0,0 +1,6 @@
+public class lesson1 {
+    public static void main(String[] args){
+        Animal cat = new Animal();
+        System.out.println(cat.iMCat());
+    }
+}
