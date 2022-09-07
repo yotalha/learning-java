1951e0cb9d25d4c60937749ef4fc114a85e82293
Talha Arshad <talha.arshad@systemsltd.com>
2022-09-08 03:56:17 +0500

    loops

---
+++ Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void forLoop() {
+        for(int i = 1; i < 10; i++){
+            System.out.println("running in for loop");
+        }
+    }
+
+    public static void whileLoop() {
+        int a = 1;
+        while(a < 10) {
+            System.out.println("running in while loop");
+            a++;
+        }
+    }
+}
