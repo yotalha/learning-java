642c8295c1c1f4e4e6f8c14caf25ed4ac75e7b43
Talha Arshad <talha.arshad@systemsltd.com>
2022-09-08 03:52:23 +0500

    if else lesson 2

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void runIfElse(){
+        int a = 5;
+        if(a == 0){
+            // Do this
+        } else if(a == 2){
+            // Do this
+        } else {
+            // Do this
+        }
+    }
+}
