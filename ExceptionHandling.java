e5cec137ac7c3c1ca540295ab1c361f5c57dc652
Talha Arshad <talha.arshad@systemsltd.com>
2022-09-08 03:59:50 +0500

    exception handling class

---
+++ ExceptionHandling.java
@@ -0,0 +1,9 @@
+public class ExceptionHandling {
+    public static void test() {
+        try{
+            // Do smething
+        } catch(Exception e){
+            // if smething goes wrong catch the exception
+        }
+    }
+}
